c78a49407cd1208f122b8aad3eb1fda5fc31f745
ahmed <ahmadghnnam60@gmail.com>
2023-01-29 07:45:19 +0300

    added a Mysql solution of Department Highest Salary

---
+++ 184.Department_Highest_Salary/184.Department_Highest_Salary.sql
@@ -0,0 +1,5 @@
+select d.name as Department , e.name as Employee,salary as Salary
+from Employee e inner join Department d
+on e.departmentId = d.id
+where (departmentId, salary) in (select departmentId, max(salary)  from Employee Group by departmentId );
+
